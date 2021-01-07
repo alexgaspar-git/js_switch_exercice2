9e68abec103729af835ec8ebc4209a2febfbafc1
Alex <agasparr98@gmail.com>
2021-01-07 12:19:47 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,62 @@
+// - # Exo 1
+//   - ## Créer une variable qui recoit un nombre via un prompt.
+//   - ## A l'aide d'un switch, affichez si le nombre est paire ou impaire:
+//     - ### Ex: 2 est nombre paire.
+
+let age = prompt("nombre?");
+
+// switch (true) {
+//     case (age % 2 == 0):
+//         console.log("c pair");
+//         break;
+//     default:
+//         console.log("spa pair");
+//         break;
+// }
+
+// - # Exo 2 
+//   - ## Créer une variable qui recoit un nombre entre 1 et 12 via un prompt.
+//   - ## A l'aide d'un switch, affichez le mois qui correspond au nombre choisi.
+//     - ## exemple: mois 1 => "Vous etes en Janvier"
+
+
+// let mois = parseInt(prompt("donne un chiffre entre 1 et 12"));
+
+// switch(mois) {
+//     case 1:
+//         console.log("vous etes en janvier");
+//         break;
+//     case 2:
+//         console.log("vous etes en fevrier");
+//         break;
+//     case 3:
+//         console.log("vous etes en mars");
+//         break;
+//     case 4:
+//         console.log("vous etes en avril");
+//         break;
+//     case 5:
+//         console.log("vous etes en mai");
+//         break;
+//     case 6:
+//         console.log("vous etes en juin");
+//         break;
+//     case 7:
+//         console.log("vous etes en juillet");
+//         break;
+//     case 8:
+//         console.log("vous etes en aout");
+//         break;
+//     case 9:
+//         console.log("vous etes en septembre");
+//         break;
+//     case 10:
+//         console.log("vous etes en octobre");
+//         break;
+//     case 11:
+//         console.log("vous etes en novembre");
+//         break;
+//     case 12:
+//         console.log("vous etes en decembre");
+//         break;        
+// }
